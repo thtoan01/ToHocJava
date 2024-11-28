bc1ab19355987a3fa0c3a0d547ae23ea9bbe3b09
Mai Thanh Toàn <thtoan01@gmail.com>
2024-11-28 10:13:04 +0700

    project

---
+++ src/ToHocJava/HelloWorld.java
@@ -0,0 +1,10 @@
+package ToHocJava;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+	}
+}
